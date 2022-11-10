a7e3f5b5d1a71709697610310b95b749e94ace7d
edemiage <117916017+edemiage@users.noreply.github.com>
2022-11-10 15:02:05 +0100

    Update MonHistoire.java

--- src/histoires/MonHistoire.java
+++ src/histoires/MonHistoire.java
@@ -1,10 +1,27 @@
 package histoires;
 
+import personnages.Human;
+import personnages.principaux.Merchant;
+import personnages.principaux.Ronin;
+import personnages.principaux.Samurai;
+import personnages.principaux.Yakuza;
+
 public class MonHistoire {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello!");
-	}
+        Human human = new Human("Prof", 10, "Porto");
+            human.sayHello();
+            human.drink();
+        Merchant commercant = new Merchant("Marchand", 35);
+            commercant.sayHello();
+        Yakuza yakuza = new Yakuza("Yaku␣le␣noir", 42,
+                "biere", "WarSong");
+            yakuza.sayHello();
+            yakuza.extort(commercant);
+        Ronin ronin = new Ronin("Roro", 61, "sake");
+            ronin.giveMoney(commercant, 10);
+            ronin.duel(yakuza);
+            ronin.sayHello();
 
+    }
 }
